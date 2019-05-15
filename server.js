83050d13a5b0092189baf00feece5ee811da9989
Jessica Chung <jchung722@gmail.com>
2019-05-15 00:41:43 -0500

    update server file

--- server.js
+++ server.js
@@ -2,3 +2,6 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/src/'));
 app.listen(process.env.PORT || 8080);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  });
