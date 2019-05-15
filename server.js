5760fdf994849428f330d933d4a711f7fe6d9a90
Jessica Chung <jchung722@gmail.com>
2019-05-15 01:38:18 -0500

    update postbuild

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/js', express.static(path.resolve('dist')));
 
 app.get('/*', function (req, res) {
     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
